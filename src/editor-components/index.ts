441a5bf73adedfbd8ef96b511268926f5089af2f
xuepingmao <410503915@qq.com>
2026-05-23 20:59:33 +0800

    feat: Compare 组件拆分横向/纵向为两个独立组件
    - Compare_DA01: 横向左右对比（flex row）
    - Compare_DA02: 纵向上下对比（flex column），type=DA02
    - parseCompare 根据 type 属性选择对应渲染器
    - 两个组件在编辑器中独立展示，example 各自体现不同布局

--- src/editor-components/index.ts
+++ src/editor-components/index.ts
@@ -23,6 +23,7 @@ import { Breaking_DA01 } from './Breaking_DA01';
 import { Steps_DA01 } from './Steps_DA01';
 import { CaseFlow_DA01 } from './CaseFlow_DA01';
 import { Compare_DA01 } from './Compare_DA01';
+import { Compare_DA02 } from './Compare_DA02';
 import { CTA_DA01 } from './Cta_DA01';
 import { Badges_DA01 } from './Badges_DA01';
 import { Statement_DA01 } from './Statement_DA01';
@@ -30,7 +31,7 @@ import { Lead_DA01 } from './Lead_DA01';
 import { Engage_DA01 } from './Engage_DA01';
 import { TimeLine_DA01 } from './TimeLine_DA01';
 
-export const components = [Title_DA01, Title_DA02, ReadingPath_DA01, PTitle, Breaking_DA01, Steps_DA01, CaseFlow_DA01, Compare_DA01, CTA_DA01, Badges_DA01, Statement_DA01, Lead_DA01, Engage_DA01, TimeLine_DA01];
+export const components = [Title_DA01, Title_DA02, ReadingPath_DA01, PTitle, Breaking_DA01, Steps_DA01, CaseFlow_DA01, Compare_DA01, Compare_DA02, CTA_DA01, Badges_DA01, Statement_DA01, Lead_DA01, Engage_DA01, TimeLine_DA01];
 
 /** 按 id 索引 */
 export const componentMap = Object.fromEntries(components.map(c => [c.id, c]));
